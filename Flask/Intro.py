c516deb8d71ad7921554ac8e8c53f48d79edc94d
Jason <leongjason822@gmail.com>
2021-01-08 12:36:09 +0800

    m

--- Flask/Intro.py
+++ Flask/Intro.py
@@ -15,7 +15,7 @@ class User(db.Model): # will have a table name of user (small u)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20),nullable=False,default='default.jpg')
     password = db.Column(db.String(60),nullable=False)
-    posts = db.relationship('Post',backref='author',lazy=True) # backref is for .lazy is for
+    posts = db.relationship('Post',backref='author',lazy='subquery') # backref is for... lazy (select,joined,dynamic,subquery)
 
     def __repr__(self):
         return f'User( {self.username}, {self.email}, {self.image_file})'
