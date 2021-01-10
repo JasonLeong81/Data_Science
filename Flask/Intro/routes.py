3109150926a4867f60a30bc5ca1afb40c84f5899
Jason <leongjason822@gmail.com>
2021-01-10 16:25:12 +0800

    m

--- Flask/Intro/routes.py
+++ Flask/Intro/routes.py
@@ -159,7 +159,7 @@ def user_posts(username):
 
 def send_reset_email(user):
     token = user.get_reset_token()
-    msg = Message('Password Reset Request',sender='noreply@demo.com',recipients=[user.email])
+    msg = Message('Password Reset Request',sender='leongjason822@gmail.com',recipients=[user.email])
     msg.body = f''' To reset your password, visit the following link: 
 {url_for('reset_token',token=token,_external=True)}
 If you did not make this request, please ignore this email.
