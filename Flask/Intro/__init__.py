d0b3622ca7c12e90c80704f03a007a643d7ee761
Jason <leongjason822@gmail.com>
2021-01-10 16:08:02 +0800

    m

--- Flask/Intro/__init__.py
+++ Flask/Intro/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import *
 from flask_login import LoginManager
-
-
+from flask_mail import Mail
+import os # for environmental variable
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'jason'
@@ -14,7 +14,12 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login' # for login required
 login_manager.login_message_category = 'info'
 
-
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'leongjason3781@gmail.com'
+app.config['MAIL_PASSWORD'] = '378100Yc'
+mail = Mail(app)
 
 
 
--- Flask/Intro/models.py
+++ Flask/Intro/models.py
@@ -1,7 +1,9 @@
 # from __main__ import db
-from Intro import db, login_manager
+from Intro import db, login_manager, app
 from datetime import datetime
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as S
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -14,6 +16,20 @@ class User(db.Model,UserMixin): # will have a table name of user (small u)
     image_file = db.Column(db.String(20),nullable=False,default='default.jpg')
     password = db.Column(db.String(60),nullable=False)
     posts = db.relationship('Post',backref='author',lazy='subquery') # backref is for... lazy (select,joined,dynamic,subquery)
+
+    def get_reset_token(self,expires_sec=1800):
+        s = S(app.config['SECRET_KEY'],expires_sec)
+        return s.dumps({'user_id':self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = S(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
 
     def __repr__(self):
         return f'User( {self.username}, {self.email}, {self.image_file})'
